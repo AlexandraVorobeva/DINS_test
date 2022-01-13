f9e3ca2ac88cb15e4c5bd1ee1ea4a2a5136b783a
Aleksandra Vorobeva <V-Sash@mail.ru>
2022-01-13 14:57:55 +0300

    programming skill

--- Programming_skills.py
+++ Programming_skills.py
@@ -5,7 +5,13 @@ city_name = input()
 appid ='2b46cc6c857f94e34ab172997c5363fc'
 
 
-def get_weather_celsius(appid, city):
+def get_weather_celsius(appid: str, city: str):
+    """
+    Get celsius temperature.
+    :param appid: key for authenticated
+    :param city: name of city
+    :return: float
+    """
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}"
     response = requests.get(url).json()
     temp = response['main']['temp']
